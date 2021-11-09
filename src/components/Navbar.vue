fef4c31fb498a2491f4b13d235187fdb9f8cb1a7
Pony <ponyma2100@gmail.com>
2021-11-09 23:20:01 +0800

    Add Navbar.vue

---
+++ src/components/Navbar.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="navbar">
+    <nav>
+      <div class="logo">
+        <router-link :to="{ name: 'Home' }">
+          <img src="../assets/taiwango.jpg" alt="taiwango" />
+        </router-link>
+      </div>
+      <div class="links">
+        <router-link :to="{ name: 'Home' }">特色活動</router-link>
+        <router-link :to="{ name: 'Home' }">美食饗宴</router-link>
+        <router-link :to="{ name: 'Home' }">精選住宿</router-link>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.navbar {
+  display: flex;
+  align-items: center;
+  width: 100%;
+  height: 4.25rem;
+  border: 1px solid black;
+}
+nav {
+  display: flex;
+  width: 100%;
+  padding: 20px;
+}
+.links {
+  display: flex;
+  justify-content: space-around;
+  width: 40%;
+  margin: auto;
+  font-weight: bolder;
+}
+</style>

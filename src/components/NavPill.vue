9f7c68cda6df0d3a9414dc3e3a41d77117db0843
Pony <ponyma2100@gmail.com>
2021-11-12 03:18:02 +0800

    Add NavPill.vue

---
+++ src/components/NavPill.vue
@@ -0,0 +1,119 @@
+<template>
+  <div class="nav-container">
+    <h1>縣市快選</h1>
+    <ul class="nav-tabs">
+      <li class="nav-item">
+        <div class="circle">
+          <img src="../assets/north.png" alt="north" />
+        </div>
+        <p>北部</p>
+        <div class="active"></div>
+      </li>
+      <li class="nav-item">中部</li>
+      <li class="nav-item">南部</li>
+      <li class="nav-item">東部</li>
+      <li class="nav-item">離島</li>
+    </ul>
+    <ul class="nav-pills">
+      <li class="nav-item">
+        <p>台北市</p>
+        <div class="active"></div>
+      </li>
+      <li class="nav-item">
+        <p>新北市</p>
+      </li>
+      <li class="nav-item">
+        <p>基隆市</p>
+      </li>
+      <li class="nav-item">
+        <p>新竹市</p>
+      </li>
+      <li class="nav-item">
+        <p>桃園市</p>
+      </li>
+      <li class="nav-item">
+        <p>新竹縣</p>
+      </li>
+      <li class="nav-item">
+        <p>宜蘭縣</p>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.nav-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 1rem;
+}
+ul {
+  display: flex;
+  list-style: none;
+}
+
+.nav-tabs > .nav-item {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 123px;
+  height: 151px;
+  left: 339px;
+  top: 344px;
+  background: #ffffff;
+  box-shadow: 0px 4px 40px rgba(0, 0, 0, 0.12);
+  border-radius: 15px;
+  margin: 1rem;
+}
+.circle {
+  width: 76px;
+  height: 76px;
+  left: 362px;
+  top: 366px;
+  background: linear-gradient(155.4deg, #78e389 0.2%, #1eb893 90.81%);
+  border-radius: 50%;
+}
+.circle img {
+  position: relative;
+  left: 12px;
+  top: 8px;
+}
+.active {
+  width: 7px;
+  height: 7px;
+  border-radius: 50%;
+  background: #c2c2c2;
+}
+.nav-pills {
+  display: flex;
+  padding: 20px;
+}
+.nav-pills > .nav-item {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-right: 10px;
+  height: 40px;
+  width: 87px;
+}
+
+.nav-pills > .nav-item > .active {
+  width: 13px;
+  height: 4px;
+  background: #10cc9f;
+  border-radius: 2.5px;
+}
+.nav-pills > .nav-item:hover {
+  width: 87px;
+  height: 40px;
+  background: #ffffff;
+  border-radius: 76px;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
     <Banner />
+    <NavPill />
     <ListView :scenicSpot="scenicSpot" />
   </div>
 </template>
@@ -9,10 +10,11 @@
 import getScenicSpot from "../composables/getScenicSpot";
 import Banner from "../components/Banner.vue";
 import ListView from "../components/ListView.vue";
+import NavPill from "../components/NavPill.vue";
 
 export default {
   name: "Home",
-  components: { Banner, ListView },
+  components: { Banner, ListView, NavPill },
 
   setup() {
     const { loadSpot, scenicSpot } = getScenicSpot();

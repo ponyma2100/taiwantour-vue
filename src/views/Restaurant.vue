29389de1bb6bd2a5127a8fd93a6e12c5a22e2de0
Pony <ponyma2100@gmail.com>
2021-11-15 04:40:27 +0800

    Add Restaurant.vue

---
+++ src/views/Restaurant.vue
@@ -0,0 +1,26 @@
+<template>
+  <NavPill />
+  <RestaurantCard :restaurants="restaurants" />
+</template>
+
+<script>
+import NavPill from "../components/NavPill.vue";
+import RestaurantCard from "../components/RestaurantyCard.vue";
+import getRestaurants from "../composables/getRestaurants";
+import { useRoute } from "vue-router";
+
+export default {
+  components: { NavPill, ActivityCard },
+
+  setup() {
+    const route = useRoute();
+    const { loadRestaurants, restaurants } = getRestaurants(route.params.city);
+    loadRestaurants(route.params.city, 30);
+
+    return { restaurants };
+  },
+};
+</script>
+
+<style>
+</style>

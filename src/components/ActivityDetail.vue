709eaf399009ad64fc8a001d3a16b06e03ee6c2e
Pony <ponyma2100@gmail.com>
2021-11-15 03:14:08 +0800

    Add ActivityDetail.vue

--- src/components/ActivityDetail.vue
+++ src/components/ActivityDetail.vue
@@ -1,28 +1,37 @@
 <template>
-  <div class="slide-spot">
+  <div class="slide-activity">
     <div class="title">
-      <p class="title-city">{{ $route.params.city }}</p>
-      <p>景點介紹</p>
+      <div class="logo">
+        <img src="../assets/activity-icon.png" alt="activity" />
+      </div>
+      <p>特色活動</p>
     </div>
-    <div class="spot-container-top">
-      <div v-for="spot in scenicSpots" :key="spot.ID">
-        <div class="spot">
-          <div class="spot-img">
+    <div class="activity-container">
+      <div v-for="activity in activities" :key="activity.ID">
+        <div class="activity">
+          <div class="activity-img">
             <img
-              :src="spot.Picture.PictureUrl1"
-              :alt="spot.Picture.PictureDescription1"
+              :src="activity.Picture.PictureUrl1"
+              :alt="activity.Picture.PictureDescription1"
             />
           </div>
-          <div class="spot-description">
-            <div class="spot-name">
-              {{ spot.Name }}
+          <div class="activity-description">
+            <div class="activity-name">
+              {{ activity.Name }}
             </div>
-            <div class="spot-detail">
-              <!-- {{ spot.Description.substring(0, 50) + "..." }} -->
-              {{ spot.DescriptionDetail.substring(0, 50) + "..." }}
+            <div class="activity-address">
+              {{ activity.Address.substring(0, 20) }}
             </div>
-            <div class="spot-address">
-              {{ spot.Address }}
+          </div>
+          <div class="activity-tag">
+            <div v-if="activity.Class1" class="activity-tag1">
+              {{ activity.Class1 }}
+            </div>
+            <div v-if="activity.Class2" class="activity-tag2">
+              {{ activity.Class2 }}
+            </div>
+            <div class="activity-city">
+              {{ activity.Address.substring(0, 3) }}
             </div>
           </div>
         </div>
@@ -32,29 +41,26 @@
 </template>
 
 <script>
-import { useRoute } from "vue-router";
-
 export default {
-  props: ["scenicSpots"],
+  props: ["activities"],
   setup(props) {
-    const route = useRoute();
+    console.log(
+      "🚀 ~ file: activityDetail.vue ~ line 40 ~ setup ~ props",
+      props
+    );
   },
 };
 </script>
 
 <style scoped>
-.spot-container-top,
-.spot-container-bottom {
+.activity-container {
   display: flex;
   align-items: center;
-  overflow: scroll;
+  /* flex-wrap: wrap; */
+  overflow-x: scroll;
   height: 400px;
 }
-
-.spot-container-bottom {
-  margin-left: 80px;
-}
-.spot {
+.activity {
   display: flex;
   flex-direction: column;
   width: 290px;
@@ -65,13 +71,13 @@ export default {
   margin: 10px 30px;
 }
 
-.spot-description {
+.activity-description {
   display: flex;
   flex-direction: column;
   padding: 10px;
 }
 
-.spot-img {
+.activity-img {
   display: flex;
   justify-content: center;
   align-items: center;
@@ -81,20 +87,20 @@ export default {
   background: #c4c4c4;
   border-radius: 10px 10px 0px 0px;
 }
-.spot-name {
+.activity-name {
   font-weight: bold;
   font-size: 16px;
   line-height: 23px;
   color: #000000;
 }
 
-.spot-address {
+.activity-address {
   font-weight: 500;
   font-size: 12px;
   line-height: 17px;
   color: #a8a8a8;
 }
-.spot-tag {
+.activity-tag {
   padding: 10px;
   display: flex;
   justify-content: space-between;
@@ -102,9 +108,9 @@ export default {
   align-items: center;
 }
 
-.spot-tag1,
-.spot-tag2,
-.spot-tag3 {
+.activity-tag1,
+.activity-tag2,
+.activity-tag3 {
   background: #ededed;
   border-radius: 11px;
   display: flex;
@@ -118,7 +124,7 @@ export default {
   padding: 3px;
 }
 
-.spot-city {
+.activity-city {
   font-size: 14px;
   line-height: 20px;
   color: #0085ff;
--- src/views/Overview.vue
+++ src/views/Overview.vue
@@ -3,30 +3,36 @@
     <Banner />
     <NavPill />
     <CityDetail :scenicSpots="scenicSpots" />
+    <ActivityDetail :activities="activities" />
   </div>
 </template>
 
 <script>
 import getScenicSpots from "../composables/getScenicSpots";
+import getActivities from "../composables/getActivities";
 import Banner from "../components/Banner.vue";
 import CityDetail from "../components/cityDetail.vue";
+import ActivityDetail from "../components/ActivityDetail.vue";
 import NavPill from "../components/NavPill.vue";
 import { onBeforeRouteUpdate, useRoute } from "vue-router";
-
 export default {
-  components: { Banner, CityDetail, NavPill },
+  components: { Banner, CityDetail, NavPill, ActivityDetail },
 
   setup() {
     const route = useRoute();
     const { loadSpots, scenicSpots } = getScenicSpots(route.params.city);
+    const { loadActivities, activities } = getActivities(route.params.city);
+
     loadSpots(route.params.city);
+    loadActivities(route.params.city);
 
     onBeforeRouteUpdate(async (to, from, next) => {
       loadSpots(to.params.city);
+      loadActivities(to.params.city);
       next();
     });
 
-    return { scenicSpots };
+    return { scenicSpots, activities };
   },
 };
 </script>
